63becb28f6273560f2fd9b3ff754c1291f63a19a
Jaya Kumar D <djayakumar571@gmail.com>
2024-02-07 09:51:25 +0530

    created exam portal backend

---
+++ ExamPortal-Backend/src/main/java/com/example/ExamPortalBackend/ExamPortalBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.ExamPortalBackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExamPortalBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExamPortalBackendApplication.class, args);
+	}
+
+}
